82802f6c9e5f7078e95d452112a7c5f004ac91f5
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-10-31 13:32:22 +0200

    Create 02. Nums 1...10.java

---
+++ First Steps In Coding/Lab/02. Nums 1...10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class MyClass{  
+    public static void main(String[] args){  
+
+        for (var i = 1; i <= 10; i++){
+
+            System.out.println(i);
+
+        }
+
+
+    }  
+}  
