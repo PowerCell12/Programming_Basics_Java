eae05f646433a66f8a6e43d3bdc856fe7b30ce6f
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-11-01 12:34:59 +0200

    Create 04. Inches to Centimeters.java

---
+++ First Steps In Coding/Lab/04. Inches to Centimeters.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class MyClass{  
+    public static void main(String[] args){  
+
+        var scanner = new Scanner(System.in);
+
+        double firstNum = Double.parseDouble(scanner.nextLine());
+
+        System.out.println(firstNum * 2.54);
+
+    }  
+}  
