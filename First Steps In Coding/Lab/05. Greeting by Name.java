179d5957541d45101b5de67f8fa3dd1094013861
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-11-01 12:35:12 +0200

    Create 05. Greeting by Name.java

---
+++ First Steps In Coding/Lab/05. Greeting by Name.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class MyClass{  
+    public static void main(String[] args){  
+
+        var scanner = new Scanner(System.in);
+
+        String firstNum = scanner.nextLine();
+
+        System.out.printf("Hello, %s!", firstNum);
+
+
+    }  
+}  
