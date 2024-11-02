e031fc2d5298bb9ca5fcaeb172b914ec8335ad66
Ivan <87208452+PowerCell12@users.noreply.github.com>
2024-11-02 12:58:08 +0200

    Create 01. USD to BGN.java

---
+++ First Steps In Coding/Exercise/01. USD to BGN.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class MyClass{  
+    public static void main(String[] args){  
+
+        var scanner = new Scanner(System.in);
+
+        double USD = Double.parseDouble(scanner.nextLine());
+
+       System.out.println(USD * 1.79549);
+         
+    }  
+}  
